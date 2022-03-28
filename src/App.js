c1b95b0434cee7d99085a2a06782e3661584d078
Huy Tu <tranhuytu242000@gmail.com>
2022-03-28 07:57:02 +0700

    init repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react'
+import { Link, Routes, Route, BrowserRouter } from 'react-router-dom'
 
-function App() {
+const Home = () => <h1>Home (Public)</h1>
+const Pricing = () => <h1>Pricing (Public)</h1>
+
+const Dashboard = () => <h1>Dashboard (Private)</h1>
+const Settings = () => <h1>Settings (Private)</h1>
+
+const Login = () => <h1>TODO</h1>
+
+function Nav() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/pricing">Pricing</Link>
+        </li>
+      </ul>
+    </nav>
+  )
 }
 
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
